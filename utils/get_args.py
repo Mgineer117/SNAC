e25493d08b5434a1e7615d897bc1b6951f058b98
Minjae Cho <whalswo117@gmail.com>
2024-11-25 16:59:08 -0600

    param updated

--- utils/get_args.py
+++ utils/get_args.py
@@ -293,7 +293,7 @@ def get_args(verbose=True):
     parser.add_argument(
         "--entropy-scaler",
         type=float,
-        default=1e-4,
+        default=1e-3,
         help="entropy scaler from PPO action-distribution",
     )
     parser.add_argument(
