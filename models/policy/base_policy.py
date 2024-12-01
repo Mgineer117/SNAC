f315a1b285632829a08764de4555d9a8a882ce5c
Minjae <whalswo117@gmail.com>
2024-12-01 14:01:11 -0600

    ss

--- models/policy/base_policy.py
+++ models/policy/base_policy.py
@@ -42,7 +42,7 @@ class BasePolicy(nn.Module):
         )
 
     def weighted_mse_loss(self, x, y):
-        weights = torch.where(y != 0.0, 50.0, 1.0)
+        weights = torch.where(y != 0.0, 200.0, 1.0)
         return torch.mean(weights * torch.pow(y - x, 2))
 
     def compute_gradient_norm(self, models, names, device, dir="None", norm_type=2):
