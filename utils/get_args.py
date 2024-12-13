48cee537e0188b94734ab0f25eb56591df961950
Minjae Cho <whalswo117@gmail.com>
2024-12-13 13:03:49 -0600

    ss

--- utils/get_args.py
+++ utils/get_args.py
@@ -423,7 +423,7 @@ def get_args(verbose=True):
     parser.add_argument(
         "--import-sf-model",
         type=bool,
-        default=True,
+        default=False,
         help="it imports previously trained model",
     )
     parser.add_argument(
