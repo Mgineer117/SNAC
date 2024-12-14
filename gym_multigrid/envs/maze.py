338c5264dbc57d091c554a2d7f2bbcd95ff505ee
Minjae Cho <whalswo117@gmail.com>
2024-12-14 10:39:17 -0600

    ss

--- gym_multigrid/envs/maze.py
+++ gym_multigrid/envs/maze.py
@@ -167,28 +167,10 @@ class Maze(MultiGridEnv):
                 fwd_pos = tuple(a + b for a, b in zip(curr_pos, (0, -1)))
                 fwd_cell = self.grid.get(*fwd_pos)
 
-                # Compute the distance between the current position and the goal
-                dist_reward = np.linalg.norm(
-                    np.array(self.goal_positions[self.grid_type]) - np.array(fwd_pos),
-                    ord=2,
-                )
-
-                # Normalize the distance with the maximum possible distance in the grid
-                dist_norm_reward = dist_reward / np.linalg.norm(
-                    [self.width, self.height], ord=2
-                )
-
-                # Invert the normalized distance to make reward larger as the agent gets closer
-                inverse_dist_reward = 1 - dist_norm_reward  # Closer => higher reward
-
-                # Scale the reward and add to the total rewards
-                rewards += (
-                    0.1 * inverse_dist_reward
-                )  # Weak reward signal, range 0 ~ 0.1
-
                 if fwd_cell is not None:
                     if fwd_cell.type == "goal":
                         done = True
+                        rewards = self._reward(i, rewards, 1)
                         info["success"] = True
                 elif fwd_cell is None or fwd_cell.can_overlap():
                     self.grid.set(*self.agents[i].pos, None)
@@ -202,28 +184,10 @@ class Maze(MultiGridEnv):
                 fwd_pos = tuple(a + b for a, b in zip(curr_pos, (0, +1)))
                 fwd_cell = self.grid.get(*fwd_pos)
 
-                # Compute the distance between the current position and the goal
-                dist_reward = np.linalg.norm(
-                    np.array(self.goal_positions[self.grid_type]) - np.array(fwd_pos),
-                    ord=2,
-                )
-
-                # Normalize the distance with the maximum possible distance in the grid
-                dist_norm_reward = dist_reward / np.linalg.norm(
-                    [self.width, self.height], ord=2
-                )
-
-                # Invert the normalized distance to make reward larger as the agent gets closer
-                inverse_dist_reward = 1 - dist_norm_reward  # Closer => higher reward
-
-                # Scale the reward and add to the total rewards
-                rewards += (
-                    0.1 * inverse_dist_reward
-                )  # Weak reward signal, range 0 ~ 0.1
-
                 if fwd_cell is not None:
                     if fwd_cell.type == "goal":
                         done = True
+                        rewards = self._reward(i, rewards, 1)
                         info["success"] = True
                 elif fwd_cell is None or fwd_cell.can_overlap():
                     self.grid.set(*self.agents[i].pos, None)
@@ -237,28 +201,29 @@ class Maze(MultiGridEnv):
                 fwd_pos = tuple(a + b for a, b in zip(curr_pos, (-1, 0)))
                 fwd_cell = self.grid.get(*fwd_pos)
 
-                # Compute the distance between the current position and the goal
-                dist_reward = np.linalg.norm(
-                    np.array(self.goal_positions[self.grid_type]) - np.array(fwd_pos),
-                    ord=2,
-                )
+                # # Compute the distance between the current position and the goal
+                # dist_reward = np.linalg.norm(
+                #     np.array(self.goal_positions[self.grid_type]) - np.array(fwd_pos),
+                #     ord=2,
+                # )
 
-                # Normalize the distance with the maximum possible distance in the grid
-                dist_norm_reward = dist_reward / np.linalg.norm(
-                    [self.width, self.height], ord=2
-                )
+                # # Normalize the distance with the maximum possible distance in the grid
+                # dist_norm_reward = dist_reward / np.linalg.norm(
+                #     [self.width, self.height], ord=2
+                # )
 
-                # Invert the normalized distance to make reward larger as the agent gets closer
-                inverse_dist_reward = 1 - dist_norm_reward  # Closer => higher reward
+                # # Invert the normalized distance to make reward larger as the agent gets closer
+                # inverse_dist_reward = 1 - dist_norm_reward  # Closer => higher reward
 
-                # Scale the reward and add to the total rewards
-                rewards += (
-                    0.1 * inverse_dist_reward
-                )  # Weak reward signal, range 0 ~ 0.1
+                # # Scale the reward and add to the total rewards
+                # rewards += (
+                #     0.1 * inverse_dist_reward
+                # )  # Weak reward signal, range 0 ~ 0.1
 
                 if fwd_cell is not None:
                     if fwd_cell.type == "goal":
                         done = True
+                        rewards = self._reward(i, rewards, 1)
                         info["success"] = True
                 elif fwd_cell is None or fwd_cell.can_overlap():
                     self.grid.set(*self.agents[i].pos, None)
@@ -271,28 +236,10 @@ class Maze(MultiGridEnv):
                 fwd_pos = tuple(a + b for a, b in zip(curr_pos, (+1, 0)))
                 fwd_cell = self.grid.get(*fwd_pos)
 
-                # Compute the distance between the current position and the goal
-                dist_reward = np.linalg.norm(
-                    np.array(self.goal_positions[self.grid_type]) - np.array(fwd_pos),
-                    ord=2,
-                )
-
-                # Normalize the distance with the maximum possible distance in the grid
-                dist_norm_reward = dist_reward / np.linalg.norm(
-                    [self.width, self.height], ord=2
-                )
-
-                # Invert the normalized distance to make reward larger as the agent gets closer
-                inverse_dist_reward = 1 - dist_norm_reward  # Closer => higher reward
-
-                # Scale the reward and add to the total rewards
-                rewards += (
-                    0.1 * inverse_dist_reward
-                )  # Weak reward signal, range 0 ~ 0.1
-
                 if fwd_cell is not None:
                     if fwd_cell.type == "goal":
                         done = True
+                        rewards = self._reward(i, rewards, 1)
                         info["success"] = True
                 elif fwd_cell is None or fwd_cell.can_overlap():
                     self.grid.set(*self.agents[i].pos, None)
