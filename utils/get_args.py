f51f2fc3d793eb1afbe64c25a5b0f13a07d09c5e
Minjae Cho <whalswo117@gmail.com>
2025-01-01 15:38:05 -0600

    ss

--- utils/get_args.py
+++ utils/get_args.py
@@ -330,7 +330,7 @@ def get_args(verbose=True):
         help="Intermediate-level model learning rate",
     )
     parser.add_argument(
-        "--op-policy-lr", type=float, default=5e-5, help="PPO-actor learning rate"
+        "--op-policy-lr", type=float, default=2e-4, help="PPO-actor learning rate"
     )
     parser.add_argument(
         "--op-critic-lr",
@@ -339,7 +339,7 @@ def get_args(verbose=True):
         help="PPO-critic learning rate. If none, BFGS is used.",
     )
     parser.add_argument(
-        "--hc-policy-lr", type=float, default=5e-5, help="PPO-actor learning rate"
+        "--hc-policy-lr", type=float, default=1e-4, help="PPO-actor learning rate"
     )
     parser.add_argument(
         "--hc-critic-lr",
@@ -348,7 +348,7 @@ def get_args(verbose=True):
         help="PPO-critic learning rate. If none, BFGS is used.",
     )
     parser.add_argument(
-        "--ppo-policy-lr", type=float, default=5e-5, help="PPO-actor learning rate"
+        "--ppo-policy-lr", type=float, default=1e-4, help="PPO-actor learning rate"
     )
     parser.add_argument(
         "--ppo-critic-lr",
