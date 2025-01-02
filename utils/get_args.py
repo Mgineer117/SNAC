00e7aeb1f687b87fffd4f7960ebc060cdd9f0a74
Minjae <whalswo117@gmail.com>
2025-01-02 17:19:04 -0600

    xx

--- utils/get_args.py
+++ utils/get_args.py
@@ -465,7 +465,7 @@ def get_args(verbose=True):
 
     # PPO parameters
     parser.add_argument(
-        "--K-epochs", type=int, default=5, help="PPO update per one iter"
+        "--K-epochs", type=int, default=10, help="PPO update per one iter"
     )
     parser.add_argument(
         "--OP-K-epochs", type=int, default=10, help="Option policy update per one iter"
