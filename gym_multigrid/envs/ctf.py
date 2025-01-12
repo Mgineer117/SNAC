6ef428370f01b8a8d427a6ba9f8f862e315407f2
Minjae Cho <whalswo117@gmail.com>
2025-01-11 18:18:20 -0600

    zxczxc

--- gym_multigrid/envs/ctf.py
+++ gym_multigrid/envs/ctf.py
@@ -850,8 +850,7 @@ class CtfMvNEnv(MultiGridEnv):
 
         info = {
             "success": self.game_stats["red_flag_captured"],
-            "failures": self.game_stats["blue_agent_defeated"]
-            or self.game_stats["blue_flag_captured"],
+            "failures": self.game_stats["blue_agent_defeated"],
             "d_ba_ra": distance_points(self.agents[0].pos, self.agents[1].pos),
             "d_ba_bf": distance_points(self.agents[0].pos, self.blue_flag),
             "d_ba_rf": distance_points(self.agents[0].pos, self.red_flag),
