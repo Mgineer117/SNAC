f6dab4f9e051a4f22ed28524d3a4ed76bb771ca4
Minjae Cho <whalswo117@gmail.com>
2025-05-26 22:49:57 -0500

    asd

--- utils/get_args.py
+++ utils/get_args.py
@@ -155,13 +155,13 @@ def get_args(verbose=True):
     parser.add_argument(
         "--seed",
         type=int,
-        default=2,  # 0, 2
+        default=3,  # 0, 2
         help="seeds for computational stochasticity --seeds 1,3,5,7,9 # without space",
     )
     parser.add_argument(
         "--num-runs",
         type=int,
-        default=2,  # 0, 2
+        default=0,  # 0, 2
         help="seeds for computational stochasticity --seeds 1,3,5,7,9 # without space",
     )
 
