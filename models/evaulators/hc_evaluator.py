296f6710cf52ccb1556979af164156630d444b77
Minjae Cho <whalswo117@gmail.com>
2025-06-09 14:17:05 -0500

    asd

--- models/evaulators/hc_evaluator.py
+++ models/evaulators/hc_evaluator.py
@@ -242,6 +242,8 @@ class HC_Evaluator(Evaluator):
                         }
                     )
 
+                    path_image = None
+                    option_image = None  # Default value
                     if num_episodes == 0 and self.gridPlot:
                         # final agent pos
                         self.get_agent_pos(env)
@@ -251,18 +253,12 @@ class HC_Evaluator(Evaluator):
 
                         # save option indices
                         option_image = self.plotOptionIndices()
-                    else:
-                        if "option_image" not in locals():
-                            option_image = None
 
+                    path_render = None
+                    # Plot if renderPlot is enabled on first episode
                     if num_episodes == 0 and self.renderPlot:
                         path_render = self.plotRender()
                         self.recorded_frames = []
-                    else:
-                        if "path_image" not in locals():
-                            path_image = None
-                        if "path_render" not in locals():
-                            path_render = None
 
         reward_list = [ep_info["ep_reward"] for ep_info in ep_buffer]
         length_list = [ep_info["ep_length"] for ep_info in ep_buffer]
