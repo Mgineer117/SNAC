ada92e3ec8dab4aa6d35877ee12ed0f15ead871d
Minjae Cho <whalswo117@gmail.com>
2025-06-17 00:14:52 -0500

    asd

--- utils/call_env.py
+++ utils/call_env.py
@@ -115,8 +115,8 @@ def call_env(args):
             observation_option=observation_option,
             territory_adv_rate=1.0,
             max_steps=args.episode_len,
-            battle_reward_ratio=0.5,
-            step_penalty_ratio=0.005,
+            battle_reward_ratio=0.25,
+            step_penalty_ratio=0.01,
         )
         disc_or_cont(env, args)
         save_dim_to_args(env, args)
